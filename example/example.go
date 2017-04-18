619f452a8a963fc913958f727075c97ff980cb2c
Peter Nguyen <peter@mictis.com>
2017-04-18 15:48:02 +0200

    Update example file to match the latest code

--- example/example.go
+++ example/example.go
@@ -1,11 +1,38 @@
-// Package example is a test package for cruder
-package example
+// Package main is a test package for cruder
+package main
 
-import "time"
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	// Postgres driver
+	_ "github.com/lib/pq"
+	"github.com/satori/go.uuid"
+)
 
 // Foo is an example struct
 type Foo struct {
-	ID        uint64    `db:"id"`
-	Name      string    `db:"name"`
-	DeletedAt time.Time `db:"deleted_at"`
+	ID        uuid.UUID  `db:"id"`
+	Name      string     `db:"name"`
+	DeletedAt *time.Time `db:"deleted_at"`
+}
+
+func main() {
+	db, err := sql.Open("postgres", "user=peter dbname=foos sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	foo := Foo{
+		Name: "Test",
+	}
+	createdFoo, err := CreateFoo(db, foo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(createdFoo)
+
+	foos, err := ListFoo(db, 0, 0, nil, nil)
+	log.Println(foos)
 }
